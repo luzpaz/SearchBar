bef6f91b3c0e0c2a6c6963a73a8291fa1f490624
Paul Ebbers <paul.ebbers@gmail.com>
2024-11-07 23:16:46 +0100

    first test

--- ResultsRefreshTools.py
+++ ResultsRefreshTools.py
@@ -1,22 +1,31 @@
 import os
-from PySide import QtGui
+from PySide6 import QtGui
 import Serialize
 
+
 def refreshToolsAction(nfo):
-  import RefreshTools
-  RefreshTools.refreshToolsAction()
+    import RefreshTools
+
+    RefreshTools.refreshToolsAction()
+
 
 def refreshToolsToolTip(nfo, setParent):
-  return Serialize.iconToHTML(genericToolIcon) + '<p>Load all workbenches to refresh this list of tools. This may take a minute, depending on the number of installed workbenches.</p>'
+    return (
+        Serialize.iconToHTML(genericToolIcon)
+        + "<p>Load all workbenches to refresh this list of tools. This may take a minute, depending on the number of installed workbenches.</p>"
+    )
 
-genericToolIcon = QtGui.QIcon(QtGui.QIcon(os.path.dirname(__file__) + '/Tango-Tools-spanner-hammer.svg'))
+
+genericToolIcon = QtGui.QIcon(QtGui.QIcon(os.path.dirname(__file__) + "/Tango-Tools-spanner-hammer.svg"))
+
+
 def refreshToolsResultsProvider():
-  return [
-    {
-      'icon': genericToolIcon,
-      'text': 'Refresh list of tools',
-      'toolTip': '',
-      'action': {'handler': 'refreshTools'},
-      'subitems': []
-    }
-  ]
+    return [
+        {
+            "icon": genericToolIcon,
+            "text": "Refresh list of tools",
+            "toolTip": "",
+            "action": {"handler": "refreshTools"},
+            "subitems": [],
+        }
+    ]
